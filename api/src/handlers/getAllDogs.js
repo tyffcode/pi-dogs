ae4efd6aa6925e84a025ec6e67620aad75af6515
tyffcode <anthoned.zg@outlook.com>
2023-05-11 11:30:03 -0500

    process one

---
+++ api/src/handlers/getAllDogs.js
@@ -0,0 +1,13 @@
+const { getAllDogsApi } = require("../controllers/getAllDogsController");
+
+const getDogsHandler = async (req, res) => {
+    try {
+        const { name } = req.body;
+        const dogs = await getAllDogsApi(name);
+        res.status(201).json(dogs);
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+module.exports = getDogsHandler;
---
+++ api/src/routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const { getAllDogs } = require('../handlers/getAllDogs');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+router.get("/dogs", getDogsHandler);
+
+module.exports = router;
