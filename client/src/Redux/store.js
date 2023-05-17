65642d0e0fd1c7ed3ae55abcfe8f20a6e9f076ae
tyffcode <anthoned.zg@outlook.com>
2023-05-17 18:06:58 -0500

    other file

---
+++ client/src/Redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose} from "redux";
+import thunkMiddleware from "redux-thunk";
+import reducer from "./reducer";
+
+const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose; //esto nos permite que nuestra aplicacion se conecte con la extencion del navegador y poder visualizar el estado global de redux en el navegador
+
+const store = createStore(
+    reducer,
+    composeEnhancer(applyMiddleware(thunkMiddleware)) //esta linea sirve para que podamos hacer peticiones a una Api/servidor
+);
+
+
+export default store
