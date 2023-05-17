54928a4ac4791fa3160ffb16e8edb0572244cf2b
tyffcode <anthoned.zg@outlook.com>
2023-05-17 17:02:26 -0500

    new file

--- api/src/controllers/getDogsById.js
+++ api/src/controllers/getDogsById.js
@@ -8,16 +8,20 @@ const { filterDataDogs } = require('../controllers/filterDataDogs');
 const getDogsById = async (req, res) => {
     const { id } =  req.params;
     try {
-        const { data } = await axios(`${API_URL}?api_key=${API_KEY}`);
-        const dog =  data.find((dog) => dog.id === +id);
-        
-        if (dog) {
-          res.status(200).json(dog);
-        } else {
-          res.status(404).json({ error: 'Perro no encontrado' });
-        }
+      const { data } = await axios(`${API_URL}?api_key=${API_KEY}`);
+      const dog =  data.find((dog) => dog.id === +id);
+
+      if (dog) {
+        res.status(200).json(dog);
+      } else {
+        const dogFromDataBase = await Dog.findByPk(id);
+        dogFromDataBase 
+        ? res.status(200).json(dogFromDataBase) 
+        : res.status(404).json({ error: 'Perro no encontrado' });
+      }
+      
     } catch (error) {
-        res.status(400).json({ error: error.message });
+      res.status(400).json({ error: error.message });
     }
 }
 
---
+++ api/src/controllers/searchByNameDog.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const { API_URL, API_KEY } = process.env;
+const axios = require('axios');
+
+searchByNameDog = () => {
+    
+}
